8223217b0e5a08ae12333ee5793bb460eac3bc25
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-03-28 11:25:41 -0700

    Final commit of this assignment

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -1,11 +1,121 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:11 PM
+ *  User: Pragya Sharma
+ *  Date: 28-Mar-20
+ *
  */
 package problem4.myqueue;
+
 // to create queue to store pre - order successor
 
+import problem1.node.TreeNode;
+
+
 public class MyQueue {
+
+    private Node front;
+
+    private Node rear;
+
+
+    public MyQueue() {
+
+        front = null;
+
+        rear = null;
+
+    }
+
+
+    public void enqueue(int data) {
+
+        Node ns = new Node();
+
+        ns.setData(data);
+
+        if (front == null) {
+
+            front = rear = ns;
+
+        } else {
+
+            rear.setNext(ns);
+
+            rear = ns;
+
+        }
+
+    }
+
+
+    public void preSuccessor(TreeNode root) {
+
+        if (root == null)
+
+            return;
+
+        else {
+
+            enqueue(root.getKey());
+
+            preSuccessor(root.getLeft());
+
+            preSuccessor(root.getRight());
+
+        }
+
+    }
+
+
+    public void getPreSucc() {
+
+        Node temp = front.getNext();
+
+        while (temp != null) {
+
+            System.out.print(temp.getData() + "\t");
+
+            temp = temp.getNext();
+
+        }
+
+    }
+
+
+    static class Node {
+
+        int data;
+
+        Node next;
+
+
+        public int getData() {
+
+            return data;
+
+        }
+
+
+        public void setData(int data) {
+
+            this.data = data;
+
+        }
+
+
+        public Node getNext() {
+
+            return next;
+
+        }
+
+
+        public void setNext(Node next) {
+
+            this.next = next;
+
+        }
+
+    }
+
 }
