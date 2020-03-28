2d0c63dd2cd1904f843dea5f2879135b34ed846b
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-03-28 11:19:31 -0700

    Final commit

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -1,11 +1,53 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
+ *  User: Pragya Sharma
+ *  Date: 28-Mar-20
+ *
  */
 package problem3.main;
-// executable class
-// use problem5.student.Student class to create object of student
+
+
+import problem3.myqueue.MyPriorityQueue;
+import problem3.node.Node;
+import problem5.student.Student;
+
+
+
+/* executable class
+
+ *use problem5.student.Student class to create object of student
+
+ */
+
+
 public class MyMain {
+
+
+    public static void main(String[] args) {
+
+        MyPriorityQueue myPriorityQueue = new MyPriorityQueue();
+
+        /*
+
+         *   Adding 5 Students by hard-coding
+
+         *   The Queue enqueues them according to ascending Roll Number as given for example in Problem Statement
+
+         */
+
+        myPriorityQueue.enqueue(new Node(new Student(15, "GLA null")));
+
+        myPriorityQueue.enqueue(new Node(new Student(500, "NK  ")));
+
+        myPriorityQueue.enqueue(new Node(new Student(50, "VD ")));
+
+        myPriorityQueue.enqueue(new Node(new Student(11, "Jio kola")));
+
+        myPriorityQueue.enqueue(new Node(new Student(55, " DBC ")));
+
+
+        myPriorityQueue.printQueue();
+
+    }
+
 }
