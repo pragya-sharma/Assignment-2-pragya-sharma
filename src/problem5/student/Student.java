577e0388919bd3205122caf5e9fc5fa8e3924a19
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-03-28 11:17:37 -0700

    Final commit

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -1,10 +1,104 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
+ *  User: Pragya Sharma
+ *  Date: 28-Mar-20
+ *
  */
 package problem5.student;
-// to store student information and properties
+
+/*
+
+ *  to store student information and properties
+
+ */
+
 public class Student {
+
+    private int rollNumber;
+
+    private String Name;
+
+    private int backLogCounter;
+
+    private int appearingCounter;
+
+
+    //  Constructor
+
+    public Student(int rollNumber, String name) {
+
+        this.rollNumber = rollNumber;
+
+        Name = name;
+
+    }
+
+
+    public Student(int rollNumber, String name, int backLogCounter, int appearingCounter) {
+
+        this.rollNumber = rollNumber;
+
+        Name = name;
+
+        this.backLogCounter = backLogCounter;
+
+        this.appearingCounter = appearingCounter;
+
+    }
+
+
+    // getter & setter methods
+
+    public int getRollNumber() {
+
+        return rollNumber;
+
+    }
+
+
+    public int getBackLogCounter() {
+
+        return backLogCounter;
+
+    }
+
+
+    public int getAppearingCounter() {
+
+        return appearingCounter;
+
+    }
+
+
+    @Override
+
+    public String toString() {
+
+        return "Student{" +
+
+                "rollNumber=" + rollNumber +
+
+                ", Name='" + Name + '\'' +
+
+                '}';
+
+    }
+
+
+    public String toStrings() {
+
+        return "Student{" +
+
+                "rollNumber=" + rollNumber +
+
+                ", Name='" + Name + '\'' +
+
+                ", backLogCounter=" + backLogCounter +
+
+                ", appearingCounter=" + appearingCounter +
+
+                '}';
+
+    }
+
 }
