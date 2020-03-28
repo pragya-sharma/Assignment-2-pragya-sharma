8f6801428703c05e9a35bcb6911ac6b33235fffe
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-03-28 11:16:15 -0700

    Final commit

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,10 +1,56 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
+ *  User: Pragya Sharma
+ *  Date: 28-Mar-20
+ *
  */
 package problem5.node;
-// to define node properties
+
+
+import problem5.student.Student;
+
+
+
+/*
+
+ *  to define node properties
+
+ */
+
 public class Node {
+
+    private Student student;
+
+    private Node next;
+
+
+    public Node(Student student) {
+
+        this.student = student;
+
+        next = null;
+
+    }
+
+
+    public Student getData() {
+
+        return student;
+
+    }
+
+
+    public Node getNext() {
+
+        return next;
+
+    }
+
+
+    public void setNext(Node next) {
+
+        this.next = next;
+
+    }
+
 }
