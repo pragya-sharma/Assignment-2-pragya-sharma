25ebfde8516dc88ec998e7453ef7909352e9590f
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-03-28 11:24:20 -0700

    problem 4 of this assignment

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,11 +1,63 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:08 PM
+ *  User: Pragya Sharma
+ *  Date: 28-Mar-20
+ *
  */
 package problem4.main;
-// executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+
+import problem1.mybst.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+
+
+/*
+
+ * > executable class
+
+ * > use problem1.mybst.MyBinarySearchTree class for creating binary tree
+
+ */
+
 public class MyMain {
+
+    public static void main(String[] args) {
+
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+
+        tree.add(51);
+
+        tree.add(39);
+
+        tree.add(31);
+
+        tree.add(54);
+
+        tree.add(92);
+
+        tree.add(42);
+
+
+        MyQueue mq = new MyQueue();
+
+        /*
+
+         *   enQueueing from the tree pre-order successors of each node
+
+         */
+
+        mq.preSuccessor(tree.root);
+
+        System.out.print("\nPre-Order successor of all the nodes :-  ");
+
+        /*
+
+         *       Printing the Queue
+
+         */
+
+        mq.getPreSucc();
+
+    }
 }
