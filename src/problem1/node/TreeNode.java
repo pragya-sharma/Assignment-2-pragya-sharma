bfae45d5e00b82d2b4fc6a7133703bc4703e5039
PRAGYA <pragya.sharma_cs18@gla.ac.in>
2020-03-28 11:06:37 -0700

    Final commit

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,59 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
+ *  User: Pragya Sharma
+ *  Date: 28-Mar-20
+ *
  */
 package problem1.node;
 
+
 public class TreeNode {
+
+    private int key;
+
+    private TreeNode left, right;
+
+
+    public TreeNode(int item) {
+
+        key = item;
+
+        left = right = null;
+
+    }
+
+
+    public int getKey() {
+
+        return key;
+
+    }
+
+
+    public TreeNode getLeft() {
+
+        return left;
+
+    }
+
+
+    public void setLeft(TreeNode left) {
+
+        this.left = left;
+
+    }
+
+
+    public TreeNode getRight() {
+
+        return right;
+
+    }
+
+
+    public void setRight(TreeNode right) {
+
+        this.right = right;
+
+    }
 }
